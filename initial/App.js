c28a5878e3d8ad7c46978d2f608fd1d7b71b3608
Riddhik <riddhikmohite@gmail.com>
2025-11-11 14:28:49 +0530

    added jsx usage

--- initial/App.js
+++ initial/App.js
@@ -1,41 +1,19 @@
-/*
-<div id=parent>
-  <div id=child>
-    <h1></h1> //siblings
-    <h2></h2> //siblings
-  </div>
-</div>
-
-ReactElement(object) =>  HTML(browser understanding language)
-*/
-
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading", xyz: "abc" },
-//   "Hello React"
-// );
-
 import React from "react";
-import {createRoot} from "react-dom/client"
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("div", { id: "siblings1" }, [
-      React.createElement("h1", {}, "I am h1 tag"),
-      React.createElement("h2", {}, "I am h2 tag"),
-    ]),
-    React.createElement(
-      "div",
-      {id:"siblings2"},
-      [React.createElement("h1", {}, "h1")],
-      React.createElement("h2", {}, "h2")
-    )
-  )
-);
-console.log(parent); //object
+import { createRoot } from "react-dom/client";
+
+// const jsx = <h1 id="heading">Hello JSX</h1>;
+
+const HeaderComponent = function () {
+  return <h1>This is Head Component inside Main Component</h1>;
+};
+const MainComponent = () => {
+  return (
+    <div className="container" id="main">
+      <HeaderComponent />
+      <h1>This is Main Component</h1>
+    </div>
+  );
+};
 const root = createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<MainComponent />);
